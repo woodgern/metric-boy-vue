a5269201d5b178b69674c28590563c85a64d31db
Nathan Woodger <woodgern@gmail.com>
2019-09-29 22:48:46 -0400

    Added base of vue app, started building `PropertyFilter` component to allow for query input

---
+++ src/components/PropertyFilter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="js-property-filter">
+    <input type="text">
+    <select name="comparators">
+      <option value="equal">=</option>
+      <option value="gt">&gt;</option>
+      <option value="lt">&lt;</option>
+      <option value="gt-eq">&gt;=</option>
+      <option value="lt-eq">&lt;=</option>
+    </select>
+    <input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PropertyFilter',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
